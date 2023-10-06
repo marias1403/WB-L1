8e9442b38e37715a0a920522d3bb0d0d047af743
Maria Arkhipova <marias1403@yandex.ru>
2023-10-06 15:13:48 +0900

    feat: Pluralize added

---
+++ Solutions/4 Pluralize.js
@@ -0,0 +1,34 @@
+// Создаем модуль с функцией изменения окончания слов
+const russianPluralize = (number, words) => {
+  // Проверяем, что передано правильное количество слов
+  if (words.length !== 3) {
+    throw new Error('Ожидается массив из трех слов');
+  }
+
+  // Определяем падеж в зависимости от остатка при делении числа на 10 и 100
+  const mod10 = number % 10;
+  const mod100 = number % 100;
+
+  // Выбираем правильное слово для соответствующего падежа и числа
+  if (mod10 === 1 && mod100 !== 11) {
+    return `${number} ${words[0]}`;
+  } else if ([2, 3, 4].includes(mod10) && ![12, 13, 14].includes(mod100)) {
+    return `${number} ${words[1]}`;
+  } else {
+    return `${number} ${words[2]}`;
+  }
+};
+
+// Экспортируем модуль
+module.exports = russianPluralize;
+
+// Импортируем модуль
+const russianPluralize = require('./russianPluralize');
+
+// Примеры использования
+console.log(russianPluralize(112, ['сообщение', 'сообщения', 'сообщений'])); // 112 сообщений
+console.log(russianPluralize(12, ['сообщение', 'сообщения', 'сообщений'])); // 12 сообщений
+console.log(russianPluralize(1, ['сообщение', 'сообщения', 'сообщений'])); // 1 сообщение
+console.log(russianPluralize(1024, ['пользователь', 'пользователя', 'пользователей'])); // 1024 пользователя
+console.log(russianPluralize(1026, ['пользователь', 'пользователя', 'пользователей'])); // 1026 пользователей
+console.log(russianPluralize(121, ['пользователь', 'пользователя', 'пользователей'])); // 121 пользователь
