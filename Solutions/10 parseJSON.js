db49b55ecf95d11cb3fa6b73991a366c32ba22ac
Maria Arkhipova <marias1403@yandex.ru>
2023-10-09 17:33:54 +0900

    fix: parseJSON

---
+++ Solutions/10 parseJSON.js
@@ -0,0 +1,55 @@
+// Условие. Реализовать функцию конвертации строки в JSON со всеми необходимыми проверками и валидацией.
+
+// Решение. Напишем функцию ручного парсинга строки
+function parseJSON(jsonString) {
+  // Удаляем пробелы
+  jsonString = jsonString.trim();
+  // Проверяем, что строка начинается с "{" и заканчивается "}"
+  if (jsonString[0] !== "{" || jsonString[jsonString.length - 1] !== "}") {
+    throw new Error('Invalid JSON: JSON object must start with "{" and end with "}"');
+  }
+  // Создаем пустой объект JSON
+  const jsonObject = {};
+  // Регулярное выражение, чтобы разбить строку JSON на пары ключ-значение.
+  // Регулярное выражение ищет соответствия вида "ключ": значение и извлекает их.
+  const regex = /"([^"]+)":\s*(("[^"]*")|(true|false|null)|(\d+\.?\d*|\.\d+)|\{[^{}]*\}|\[[^\[\]]*\])/g;
+  // В цикле while происходит поиск всех соответствий регулярному выражению в строке jsonString.
+  // Каждое соответствие обрабатывается и добавляется в объект jsonObject.
+  let match;
+  while ((match = regex.exec(jsonString)) !== null) {
+    // Извлекаем ключ
+    const key = match[1];
+    let value = match[2];
+
+    if (value.charAt(0) === '"' && value.charAt(value.length - 1) === '"') {
+      value = value.slice(1, -1);
+    }
+    // Значение обрабатывается в зависимости от его типа
+    if (value === 'null') {
+      jsonObject[key] = null;
+    } else if (value === 'true') {
+      jsonObject[key] = true;
+    } else if (value === 'false') {
+      jsonObject[key] = false;
+    } else if (!isNaN(value)) {
+      jsonObject[key] = +value;
+    } else if (value.startsWith('{') && value.endsWith('}')) {
+      // Рекурсия, если значение является объектом
+      jsonObject[key] = parseJSON(value);
+    } else if (value.startsWith('[') && value.endsWith(']')) {
+      jsonObject[key] = value.slice(1, -1).split(',').map((item) => item.trim().replace(/["']/g, ''));
+    } else {
+      jsonObject[key] = value;
+    }
+  }
+  return jsonObject;
+}
+// Строка для парсинга
+const string = '{"name":"Maria","age":25,"hobbies":["reading","drawing"],"isStudent":true,"address":{"city":"Moscow","country":"Russia"}}';
+// Используем конструкцию "try...catch" для обработки ошибок при их наличии
+try {
+  const parsedJSON = parseJSON(string);
+  console.log(parsedJSON);
+} catch (error) {
+  console.error('Error:', error.message);
+}
