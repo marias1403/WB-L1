0f180386c66b6084da3137fa1745267703ddf3c6
Maria Arkhipova <marias1403@yandex.ru>
2023-10-16 21:56:43 +0900

    fix: comments

--- solutions/15 async.js
+++ solutions/15 async.js
@@ -2,6 +2,8 @@
 // ключевое слово await для ожидания выполнения других асинхронных операций, и возвращает результат выполнения.
 
 // Решение.
+// Асинхронная функция, которая ожидает выполнения двух других асинхронных операций.
+// В случае успешного выполнения этих операций, она возвращает объект с результатами
 async function waitForAsyncOperations() {
   try {
     const result1 = await asyncOperation1();
@@ -13,7 +15,9 @@ async function waitForAsyncOperations() {
     throw error;
   }
 }
-
+// Асинхронная операция, представленная в виде функции, которая возвращает промис
+// и имитирует задержку в 1 секунду с помощью setTimeout.
+// После истечения времени она выполняет промис, возвращая строку.
 async function asyncOperation1() {
   return new Promise((resolve) => {
     setTimeout(() => {
@@ -21,7 +25,7 @@ async function asyncOperation1() {
     }, 1000);
   });
 }
-
+// Аналогичная первой функции
 async function asyncOperation2() {
   return new Promise((resolve) => {
     setTimeout(() => {
@@ -29,7 +33,8 @@ async function asyncOperation2() {
     }, 1000);
   });
 }
-
+// Здесь вызывается waitForAsyncOperations(), и после выполнения всех асинхронных операций,
+// либо успешно, либо с ошибкой, применяются методы then и catch для обработки результата или ошибки
 waitForAsyncOperations()
   .then((result) => {
     console.log("Результат выполнения асинхронной функции:", result);
